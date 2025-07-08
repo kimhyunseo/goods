b9465a944bbd49d237df30bb2c066f052faaeb39
yull03 <rlaaudtjr67@gmail.com>
2025-07-08 17:54:31 +0900

    crt

--- src/MainPage/Footer.js
+++ src/MainPage/Footer.js
@@ -3,29 +3,29 @@
 const Footer = () => {
     return (
         <footer>
-            <div>
+            <div className="top-text">
                 <p><a href="#">회사 소개</a></p>
                 <p><a href="#">개인정보 처리 방침</a></p>
                 <p><a href="#">이용 약관</a></p>
                 <p><a href="#">고객 센터</a></p>
             </div>
-            <div>
+            <div className="icon">
                 <p><i></i></p>
                 <p><i></i></p>
                 <p><i></i></p>
             </div>
-            <div>
+            <div className="middle-text">
                 <p>고객센터</p>
                 <h2>1588-0000</h2>
                 <p>평일: 09:00 ~ 18:00</p>
                 <p>E-mail : customer@artbox.co.kr</p>
             </div>
-            <div>
+            <div className="bottom-text">
                 <p>대표자: ㅇㅇㅇ 사업자 등록번호 | 123-456-7890 <br/>
                 주소 | 서울특별시 강남구 테헤란로 123, 7층<br/>
                 통신판매업신고 | 제2025-서울강남-1234호</p>
             </div>
-            <div>
+            <div className="foot-text">
                 <img/>
                 <p>ⓒ 2025 [Ririm].<br/>
                 All rights reserved.</p>
