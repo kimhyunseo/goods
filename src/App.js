022c0c11100d2a30fc3f9ef359949ac3f192cadf
junhyoung2 <jun83467@gmail.com>
2025-07-09 12:46:56 +0900

    0709 카테고리 페이지

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Category from './components/Category'; // 상대 경로 기준
 
 const App = () => {
   return (
     <div>
       
+      <Category />
     </div>
   );
 };
